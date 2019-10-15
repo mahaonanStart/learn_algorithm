e372aefcf1bcf3c26b1981ef13ad25178ef92fd1
mahaonan <mahaonan@pwrd.com>
2019-10-15 18:38:36 +0800

    组合模式

---
+++ src/design_pattern/composite/practice/Product.java
@@ -0,0 +1,17 @@
+package design_pattern.composite.practice;
+
+/**
+ * @Author: M˚Haonan
+ * @Date: 2019-10-15 18:23
+ * @Description: 抽象构件 物品
+ */
+public interface Product {
+
+    /**
+     */
+    float calculate();
+
+    /**
+     */
+    void show();
+}
--- src/design_pattern/composite/simple/CompositePattern.java
+++ src/design_pattern/composite/simple/CompositePattern.java
@@ -18,11 +18,11 @@ interface Component {
 }
 
 //树叶构件
-class leaf implements Component {
+class Leaf implements Component {
 
     private String name;
 
-    public leaf(String name) {
+    public Leaf(String name) {
         this.name = name;
     }
 
@@ -78,5 +78,18 @@ class Composite implements Component {
 
 public class CompositePattern {
 
+    public static void main(String[] args)
+    {
+        Component c0=new Composite();
+        Component c1=new Composite();
+        Component leaf1=new Leaf("1");
+        Component leaf2=new Leaf("2");
+        Component leaf3=new Leaf("3");
+        c0.add(leaf1);
+        c0.add(c1);
+        c1.add(leaf2);
+        c1.add(leaf3);
+        c0.operation();
+    }
 
 }
